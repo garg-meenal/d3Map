a34a361b46010c093647663dadc474930ee9ebbd
Meenal Garg <meenalgarg0305@gmail.com>
2023-05-10 18:31:17 +0530

    mapbox black dots

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorldMapComponent } from './world-map/world-map.component';
 import { UsaMapComponent } from './usa-map/usa-map.component';
+import { MapboxComponent } from './mapbox/mapbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WorldMapComponent,
-    UsaMapComponent
+    UsaMapComponent,
+    MapboxComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mapbox/mapbox.component.ts
@@ -0,0 +1,157 @@
+import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import * as d3 from 'd3v4';
+
+@Component({
+  selector: 'app-mapbox',
+  templateUrl: './mapbox.component.html',
+  styleUrls: ['./mapbox.component.scss']
+})
+export class MapboxComponent implements OnInit {
+
+  map: mapboxgl.Map;
+  //style = 'mapbox://styles/mapbox/light-v11';
+  style= 'mapbox://styles/mapbox/streets-v9';
+  //style = 'mapbox://styles/mapbox/terrain-v9';
+
+  parks = {
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Lincoln Park",
+          "description": "A northside park that is home to the Lincoln Park Zoo"
+        },
+        "geometry": {
+          "coordinates": [-87.637596, 41.940403],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Burnham Park",
+          "description": "A lakefront park on Chicago's south side"
+        },
+        "geometry": {
+          "coordinates": [-87.603735, 41.829985],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Millennium Park",
+          "description": "A downtown park known for its art installations and unique architecture"
+        },
+        "geometry": {
+          "coordinates": [-87.622554, 41.882534],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Grant Park",
+          "description": "A downtown park that is the site of many of Chicago's favorite festivals and events"
+        },
+        "geometry": {
+          "coordinates": [-87.619185, 41.876367],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Humboldt Park",
+          "description": "A large park on Chicago's northwest side"
+        },
+        "geometry": {
+          "coordinates": [-87.70199, 41.905423],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Douglas Park",
+          "description": "A large park near in Chicago's North Lawndale neighborhood"
+        },
+        "geometry": {
+          "coordinates": [-87.699329, 41.860092],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Calumet Park",
+          "description": "A park on the Illinois-Indiana border featuring a historic fieldhouse"
+        },
+        "geometry": {
+          "coordinates": [-87.530221, 41.715515],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Jackson Park",
+          "description": "A lakeside park that was the site of the 1893 World's Fair"
+        },
+        "geometry": {
+          "coordinates": [-87.580389, 41.783185],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "title": "Columbus Park",
+          "description": "A large park in Chicago's Austin neighborhood"
+        },
+        "geometry": {
+          "coordinates": [-87.769775, 41.873683],
+          "type": "Point"
+        }
+      }
+    ],
+    "type": "FeatureCollection"
+  };
+  
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.map = new mapboxgl.Map({
+      accessToken:
+        'pk.eyJ1IjoiZHBpZXRyb2NhcmxvIiwiYSI6ImNram9tOGFuMTBvb3oyeXFsdW5uYmJjNGQifQ._zE6Mub0-Vpl7ggMj8xSUQ',
+      container: 'map',
+      style: this.style,
+      center: [-95.2907462468837, 39.0911397651497], // starting position
+      zoom: 4, // starting zoom
+    });
+
+    
+
+    this.map.on('load', () => {
+      this.map.addLayer({
+        id: 'circle-layer',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: this.parks.features
+          }
+        },
+        paint: {
+          'circle-color': 'black',
+          'circle-radius':10
+        }
+      });
+    });
+  }
+  
+
+}
